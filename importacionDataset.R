3021218dd2cdc7706171c695ac5676100b6b2f50
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2025-11-22 19:54:53 -0500

    Reportes

--- importacionDataset.R
+++ importacionDataset.R
@@ -1,3 +1,6 @@
 library(readr)
 icfes_20201 <- read_csv("icfes_20201.csv")
 View(icfes_20201)
+names(icfes_20201)
+unique(icfes_20201$ESTU_COD_MCPIO_PRESENTACION)
+sum(is.na(icfes_20201$COLE_COD_DANE_SEDE))
